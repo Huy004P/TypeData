6d6fbe12e9d52d58d302878bbbe0723e2b06a0a5
Huy004P <114895245+Huy004P@users.noreply.github.com>
2022-10-06 10:06:42 +0700

    Add files via upload

---
+++ src/basic/dev/MainApp.java
@@ -0,0 +1,42 @@
+package basic.dev;
+
+public class MainApp {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		String ten = "Pham Nguyen Dinh Huy";
+		String maSinhVien = "2260197";
+		String cmnd = "046204000";
+		String tenLop = "Tin Hoc Ung Dung";
+		String tenTruong = "Hueic";
+		String diemTrungBinh = "9.5";
+		System.out.println("Thong Tin Mot Sinh Vien");
+		System.out.println("Ten:" + ten);
+		System.out.println("Ma Sinh Vien:" + maSinhVien);
+		System.out.println("CMND:" + cmnd);
+		System.out.println("Ten Lop:" + tenLop);
+		System.out.println("Ten Truong:" + tenTruong);
+		System.out.println("Diem Trung Binh:" + diemTrungBinh);
+		
+		String tenCN = "Pham Nguyen Dinh Huy";
+		String diaChi = "119/115 Truong Chinh";
+		String sdt = "0346455";
+		String heSoLuong = "7000000";
+		System.out.println("Thong Tin Cong Nhan");
+		System.out.println("Ten CN:" + tenCN);
+		System.out.println("Dia Chi:" + diaChi);
+		System.out.println("SDT:" + sdt);
+		System.out.println("He So Luong:" + heSoLuong);
+		
+		String tenXe = "Lamborghini";
+		String hang = "Lambor";
+		String gia = "120000000000";
+		String mauSon = "Green";
+		System.out.println("Thong Tin Mot Chiec Xe Car");
+		System.out.println("Ten Xe:" + tenXe);
+		System.out.println("Hang:" + hang);
+		System.out.println("Gia:" + gia);
+		System.out.println("Mau Son:" + mauSon);
+
+	}
+}
